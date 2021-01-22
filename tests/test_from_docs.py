95dda02c576f1195742560cc5b4bd2893f5d751f
Olof Bjarnason <olof.bjarnason@proagile.se>
2021-01-22 19:37:48 +0100

    Move encode/decode to src/

--- tests/test_from_docs.py
+++ tests/test_from_docs.py
@@ -1,38 +1,12 @@
 from hypothesis import given, example
 from hypothesis.strategies import text
 
+from src.encode_decode import encode, decode
+
 
 @given(text())
 @example("")
 def test_decode_inverts_encode(s):
     assert decode(encode(s)) == s
-
-
-
-def encode(input_string):
-    if not input_string:
-        return ""
-    count = 1
-    prev = ""
-    lst = []
-    for character in input_string:
-        if character != prev:
-            if prev:
-                entry = (prev, count)
-                lst.append(entry)
-            count = 1
-            prev = character
-        else:
-            count += 1
-    entry = (character, count)
-    lst.append(entry)
-    return lst
-
-
-def decode(lst):
-    q = ""
-    for character, count in lst:
-        q += character * count
-    return q
 
 
